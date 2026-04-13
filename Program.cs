9da899b03b1b82a30322e002572c27f5f1bf7934
elinovic <elijus.novickij@sa.stud.vu.lt>
2026-04-13 18:56:44 +0300

    Trecia uzduotis

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+
+class Program
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+    static void Main()
+    {
+        string failas = File.ReadAllText("user.json");
+
+        List<User> zmones = JsonConvert.DeserializeObject<List<User>>(failas); //reikia list kad kelis zmones nuskaityt
+
+        foreach (var zmogus in zmones)
+        {
+            Console.WriteLine($"Name: {zmogus.Name}, Age: {zmogus.Age}, City: {zmogus.City}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JsonNaudojimas
 
-Versija 1.0: Pirma užduotis, sukurtas json failas.
+Versija 1.1: Sukurta pirma programos versija - JSON failo nuskaitymas ir išvedimas į console.
+
+Versija 1.0: Pirma ir antra užduotis, sukurtas ir papildytas JSON failas.
